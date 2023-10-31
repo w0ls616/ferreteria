264d4a3c73d4dbd9dd17e7f689b662dabe27a3c8
wilber <wilber@DESKTOP-04AON15>
2023-10-31 09:45:51 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/ferreteria/Ferreteria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ferreteria;
+
+/**
+ *
+ * @author wilber
+ */
+public class Ferreteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
